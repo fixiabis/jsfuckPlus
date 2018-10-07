2bf40d22175e5abcc1f3d691f26737bf63863893
fixiabis <benkao0000@gmail.com>
2018-10-07 23:32:19 +0800

    update

---
+++ js/4cjf-transpile.js
@@ -0,0 +1,54 @@
+var input = document.getElementById("input");
+var output = document.getElementById("output");
+var execute = document.getElementById("execute");
+var uglify = document.getElementById("uglify");
+
+input.addEventListener("input", function () {
+    output.value = transpile(input.value, 0);
+});
+
+execute.addEventListener("click", function () {
+    eval(output.value);
+});
+
+uglify.addEventListener("click", function () {
+    output.value = transpile(input.value, 1);
+});
+
+function transpile(strs, m) {
+    strs = strs.split("").map(
+        c => (
+            (c = c.charCodeAt().toString(16)),
+            c.length < 4 && (c = "0".repeat(4 - c.length) + c),
+            c
+        )
+    ).join("");
+
+    var r = "__[_____]";
+    
+    if (m == 0) {
+        for (var str of strs)
+            r += "\n" + (function (str) {
+                var _ = ["_", "__", "___", "____"],
+                    r = "";
+                for (var c of str) {
+                    c = parseInt(c, 16);
+                    r += `${_[(c / 4) | 0]}[${_[c % 4]}]`;
+                }
+                return r;
+            })(str);
+        return r + "\n_[_____]";
+    } else {
+        for (var str of strs)
+            r = (function (str, cat) {
+                var _ = ["_", "__", "___", "____"],
+                    r = "";
+                for (var c of str) {
+                    c = parseInt(c, 16);
+                    r = `${_[(c / 4) | 0]}[${((r && r + ",") || cat + ",")}${_[c % 4]}]`;
+                }
+                return r;
+            })(str, r);
+        return `_[${r},_____]`;
+    }
+}
